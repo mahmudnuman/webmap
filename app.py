e7256d0c281f35754c29a91d771006f114a18c1f
mahmudnuman <mahmudnuman@gmail.com>
2022-11-28 00:57:01 +0600

    map project done with layer control

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
 import folium
-map=folium.Map(location=[23.82368, 90.3605], zoom_start=12, tiles="OpenStreetMap")
+import pandas as pd
 
-feature_group=folium.FeatureGroup(name="Python Practice Maps")
+data=pd.read_csv("Volcanoes.txt")
 
-coordinates=[[23.823783751620876, 90.36045035484473],
- [23.822838343980305, 90.36355074522358],
- [23.822779985252616, 90.36194951480202]]
+lattitude=data["LAT"]
+longitude=data["LON"]
+information= "Name:" + data["NAME"].astype(str) + "\nLocation:"+data["LOCATION"].astype(str) +"\nStatus:" + data["STATUS"].astype(str) + "\nType:" +data["TYPE"].astype(str) + "\nElevation:" + data["ELEV"].astype(str) + ' m'
+elevetion=data["ELEV"]
 
-for coordinate in coordinates:
+def color_producer(elev):
+    if elev < 1000:
+        return "green"
+    elif 1000 <= elev <3000:
+        return "orange"
+    elif 3000 <= elev <5000:
+        return "pink"
+    else:
+        return "red"
+        
     
 
-    feature_group.add_child(folium.Marker(location=coordinate,
-                            popup="Hello I am here",
-                            icon=folium.Icon(color="pink")))
+map=folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="OpenStreetMap")
 
-map.add_child(feature_group)
+fgv=folium.FeatureGroup(name="Volcanos in USA")
+fgp=folium.FeatureGroup(name="Population")
+
+
+for lat,lon,info,ele in zip(lattitude,longitude,information,elevetion):
+    
+
+    fgv.add_child(folium.CircleMarker(location=[lat,lon],
+                            radius=7,
+                            popup=str(info),
+                            fill_color=color_producer(ele), color ="gray",
+                            fill_opacity=0.7))
+
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',
+                                    encoding='utf-8-sig').read(),
+                                   style_function=lambda x:{
+                                                        'fillColor':'green'
+                                                         if x['properties']['POP2005'] <10000000 
+                                                         else "orange"
+                                                         if 100000000<= x['properties']['POP2005'] <200000000 
+                                                         else "red"
+                                                            }
+                                       ))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("map1.html");
